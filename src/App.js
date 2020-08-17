2b748279a1a08b49ee513802eae2b3c56b2181d2
ikeng <satidasss@gmail.com>
2020-08-17 17:21:58 +0700

    part1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Container } from "@material-ui/core";
+import Header from "./components/fragments/Header";
+import Menu from "./components/fragments/Menu";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    padding: theme.spacing(20)
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+  const [count, setCount] = React.useState(0);
+
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header handleDrawerOpen={handleDrawerOpen} open={openDrawer} count={count} setCount={setCount} />
+      <Menu open={openDrawer} handleDrawerClose={handleDrawerClose} />
+      <Container className={classes.content}>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route
+            exact={true}
+            path="/"
+            component={() => <Redirect to="/login" />}
+          />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
-
-export default App;
