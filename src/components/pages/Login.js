fb435de3146172e18f3232829adcd359486ed932
ikeng <satidasss@gmail.com>
2020-08-18 21:45:23 +0700

    part2

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 352,
   },
   media: {
-    height: 140,
+    height: 200,
+  },
+  form: {
+    width: 320,
   },
 });
 
@@ -22,30 +26,48 @@ export default function Login() {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
         <CardMedia
-          className={classes.media}
-          image={`${process.env.PUBLIC_URL}/images/authen_header.jpg`}
-          title="Contemplative Reptile"
+            className={classes.media}
+            height={200}
+            image={`${process.env.PUBLIC_URL}/images/authen_header.jpg`}
+            title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
+            <Typography gutterBottom variant="h5" component="h2">
+            Login
+            </Typography>
+            <form className={classes.form} noValidate autoComplete="off">
+            <TextField
+                id="username"
+                label="Username"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                autoComplete="email"
+                autoFocus
+                required
+            />
+            <TextField
+                id="password"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                autoComplete="current-password"
+                autoFocus
+                required
+            />
+            </form>
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
+
+        <CardActions>
+            <Button size="small" color="primary">
+            Submit
+            </Button>
+            <Button size="small" color="primary">
+            Cancel
+            </Button>
+        </CardActions>
     </Card>
   );
 }
